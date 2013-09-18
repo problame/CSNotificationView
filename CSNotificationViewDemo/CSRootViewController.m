2f095ee39915ef0e7ae41149da1cd3f143c12a42
Christian Schwarz <me@cschwarz.com>
2013-09-18 21:22:51 +0200

    Code cosmetics.

--- CSNotificationViewDemo/CSRootViewController.m
+++ CSNotificationViewDemo/CSRootViewController.m
@@ -16,14 +16,23 @@
 @implementation CSRootViewController
 
 - (IBAction)showError:(id)sender {
-    [CSNotificationView showInViewController:self style:CSNotificationViewStyleError message:@"A critical error happened."];
+    [CSNotificationView showInViewController:self
+                                       style:CSNotificationViewStyleError
+                                     message:@"A critical error happened."];
 }
 - (IBAction)showSuccess:(id)sender {
-    [CSNotificationView showInViewController:self style:CSNotificationViewStyleSuccess message:@"Great, it works."];
+    [CSNotificationView showInViewController:self
+                                       style:CSNotificationViewStyleSuccess
+                                     message:@"Great, it works."];
 }
 
 - (IBAction)showCustom:(id)sender {
-    [CSNotificationView showInViewController:self tintColor:[UIColor colorWithRed:0.000 green:0.6 blue:1.000 alpha:1] image:nil message:@"No icon and a message that needs two rows and extra presentation time to be displayed properly." duration:5.8f];
+    [CSNotificationView showInViewController:self
+            tintColor:[UIColor colorWithRed:0.000 green:0.6 blue:1.000 alpha:1]
+                image:nil
+              message:@"No icon and a message that needs two rows and extra \
+                        presentation time to be displayed properly."
+             duration:5.8f];
 }
 
 
