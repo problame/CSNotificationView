34b7421288442fcb039cf734eaa15fe671b268f7
Ben <b.cylin@gmail.com>
2014-04-15 22:01:33 +0800

    Manually adjustsFontSizeToFitWidth in iOS 6

--- CSNotificationView/CSNotificationView.m
+++ CSNotificationView/CSNotificationView.m
@@ -294,6 +294,37 @@ static NSString * kCSNavigationBarBoundsKeyPath = @"bounds";
     [super updateConstraints];
 }
 
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] < 7) {
+        //Manually adjustsFontSizeToFitWidth in iOS 6
+        static CGFloat defaultPoint;
+        static CGFloat minimumPoint;
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            defaultPoint = self.textLabel.font.pointSize;
+            minimumPoint = ceilf(defaultPoint * self.textLabel.minimumScaleFactor);
+        });
+
+        UIFont *font = self.textLabel.font;
+        CGSize constrainedSize = CGSizeMake(self.textLabel.frame.size.width, CGFLOAT_MAX);
+
+        for (NSInteger point = defaultPoint; point >= minimumPoint; point--) {
+            font = [font fontWithSize:point];
+            CGSize fitSize = [self.textLabel.text sizeWithFont:font constrainedToSize:constrainedSize lineBreakMode:NSLineBreakByTruncatingTail];
+            if (fitSize.height <= CGRectGetHeight(self.textLabel.frame)) {
+                break;
+            }
+        }
+        self.textLabel.font = font;
+
+        //Call super -layoutSubviews again to avoid 'NSInternalInconsistencyException'
+        [super layoutSubviews];
+    }
+}
+
 - (void)setFrame:(CGRect)frame
 {
     [super setFrame:frame];
