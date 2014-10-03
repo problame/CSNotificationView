6417fcbaa0909d39c6c9ce7ce7f039d5826ce7ab
Christian Schwarz <me@cschwarz.com>
2014-10-04 00:13:33 +0200

    Added class method to get the images used internally for `error` and `success` style.

--- CSNotificationView/CSNotificationView.h
+++ CSNotificationView/CSNotificationView.h
@@ -48,6 +48,10 @@ typedef void(^CSVoidBlock)();
                                                           image:(UIImage*)image
                                                         message:(NSString*)message;
 
+#pragma mark + icons
+
++ (UIImage*)imageForStyle:(CSNotificationViewStyle)style;
+
 #pragma mark - initialization
 
 - (instancetype)initWithParentViewController:(UIViewController*)viewController;
